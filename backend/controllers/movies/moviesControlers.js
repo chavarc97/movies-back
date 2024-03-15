f35c627e23ec824bc7b135e0299fa2d33b12e0e1
Salvador Rodriguez <chava.crb@gmail.com>
2024-03-14 19:43:40 -0600

    Fix: createMovie controller and updateVotes. Cors added

--- backend/controllers/movies/moviesControlers.js
+++ backend/controllers/movies/moviesControlers.js
@@ -7,21 +7,25 @@ const getMovies = asyncHandler(async (req, res) => {
 });
 
 const createMovie = asyncHandler(async (req, res) => {
-  if (!req.body.name || !req.body.description) {
-    res.status(400);
-    throw new Error("Please Fill all the fields");
-  }
-  const movie = await Movie.create({
+  const movieData = {
+    adult: req.body.adult,
+    backdrop_path: req.body.backdrop_path,
+    genre_ids: req.body.genre_ids, // Array of numbers
     original_language: req.body.original_language,
     original_title: req.body.original_title,
     overview: req.body.overview,
-    popularity: req.body.popularity,
+    popularity: req.body.popularity || 0, // Initialize to 0 if not provided
     poster_path: req.body.poster_path,
-    release_date: req.body.release_date,
+    release_date: new Date(req.body.release_date),
     title: req.body.title,
-    vote_average: req.body.vote_average,
-    vote_count: req.body.vote_count,
-  });
+    vote_average: req.body.vote_average || 0, // Initialize to 0 if not provided
+    vote_count: req.body.vote_count || 0, // Initialize to 0 if not provided
+  };
+  if (!movieData) {
+    res.status(400);
+    throw new Error("Please Fill all the fields");
+  }
+  const movie = await Movie.create(movieData);
   res.status(201).json({ movie });
 });
 
@@ -31,27 +35,35 @@ const deleteMovie = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Movie not found");
   }
-  await Movie.deleteOne(movie)
-  res.status(200).json({if: req.params.id, message: "Movie removed"});
+  await Movie.deleteOne(movie);
+  res.status(200).json({ if: req.params.id, message: "Movie removed" });
 });
 
 const updateVotes = asyncHandler(async (req, res) => {
-    const movie = await Movie.findById(req.params.id);
-            if (!movie) {
-                res.status(404);
-                throw new Error("Movie not found");
-            }
-            const movieUpdated = await Movie.updateOne(
-                { _id: req.params.id },
-                { $set: { vote_average: req.body.vote_average, vote_count: req.body.vote_count } },
-                { new: true }
-            );
-        res.status(200).json(movieUpdated);
-    })
+  const { vote } = req.body;
 
-module.exports = { 
-    getMovies,
-    createMovie, 
-    deleteMovie, 
-    updateVotes ,
+  // Buscar la película existente por su ID
+  const movie = await Movie.findById(req.params.id);
+
+  if (!movie) {
+    res.status(404);
+    throw new Error("Movie not found");
+  }
+
+  // Agregar el voto proporcionado al vote_count y recalcular el vote_average
+  movie.vote_count += 1;
+  movie.vote_average =
+    (movie.vote_average * (movie.vote_count - 1) + vote) / movie.vote_count;
+
+  // Guardar los cambios en la base de datos
+  await movie.save();
+
+  res.status(200).json(movie);
+});
+
+module.exports = {
+  getMovies,
+  createMovie,
+  deleteMovie,
+  updateVotes,
 };
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config()
-const connectDB = require('./config/db')
-const cors = require('cors');
-const { loadMovies } = require('./populate/loadMovies')
-const { errorHandler } = require('./middleware/errorHandler')
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
+const cors = require("cors");
+const { loadMovies } = require("./populate/loadMovies");
+const { errorHandler } = require("./middleware/errorHandler");
 const port = process.env.PORT || 5000;
 
 // Conexión a la base de datos
@@ -12,30 +12,31 @@ connectDB();
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // routes
-app.use('/api/movies', require('./routes/moviesRoutes'))
-app.use('/api/users', require('./routes/usersRoutes'))
+app.use("/api/movies", require("./routes/moviesRoutes"));
+app.use("/api/users", require("./routes/usersRoutes"));
 
 // error handler
-app.use(errorHandler)
+app.use(errorHandler);
 
 async function serverStart() {
-    try {
-      // Llama a la función para cargar películas
-     // await loadMovies();
-  
-      // Inicia el servidor
-      app.listen(port, () => console.log(`Servidor iniciado en el puerto ${port}`))
+  try {
+    // Llama a la función para cargar películas
+    // await loadMovies();
 
-    } catch (error) {
-      console.error('Error al iniciar el servidor:', error);
-    }
+    // Inicia el servidor
+    app.listen(port, () =>
+      console.log(`Servidor iniciado en el puerto ${port}`)
+    );
+  } catch (error) {
+    console.error("Error al iniciar el servidor:", error);
   }
+}
 
-
-
-  serverStart()
-
+// start server
+serverStart();
