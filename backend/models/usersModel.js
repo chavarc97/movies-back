d2592676d72a7c33c9876b174e2a8b5fce15b165
Salvador Rodriguez <chava.crb@gmail.com>
2024-03-25 18:53:31 -0600

    userModel actualization

--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -12,20 +12,11 @@ const userSchema = mongoose.Schema(
       type: String,
       required: [true, "Please provide your email address"],
       unique: [true, 'This email is already registered'],
-      match: [/^\S+@\S+\.\S+$/, 'Please provide a valid email address']
     },
     password: {
       type: String,
       required: [true, "Please provide your password"],
     },
-    isAdmin: {
-      type: Boolean,
-      required: [true, "Please provide if the user is an admin"],
-    },
-    active: {
-      type: Boolean,
-      default: true,
-    }
   },
   {
     timestamps: true,
